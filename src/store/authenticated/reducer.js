31b4cfa39556edc51aba160ffe52899cfd2e72dd
wilmer <wilmer.payall@gmail.com>
2019-01-27 23:30:22 -0100

    Separate Login Reducer

--- src/store/authenticated/reducer.js
+++ src/store/authenticated/reducer.js
@@ -6,34 +6,32 @@ import { fromJS } from 'immutable';
 import { GET_AUTHENTICATED_USER_ASYNC } from './types';
 
 const initialState = fromJS({
-  authenticated: {
-    isFetching: true,
-    fetched: false,
-    fetchError: false,
-    errorMessage: '',
-  },
+  isFetching: true,
+  fetched: false,
+  fetchError: false,
+  errorMessage: '',
 });
 
 const reducer = typeToReducer({
   [GET_AUTHENTICATED_USER_ASYNC]: {
-    PENDING: state => state.mergeIn(['authenticated'], {
+    PENDING: state => state.merge({
       fetched: false,
       isFetching: true,
       fetchError: false,
     }),
-    ERROR: (state, { payload: { errors } }) => state.mergeIn(['authenticated'], {
+    ERROR: (state, { payload: { errors } }) => state.merge({
       fetched: false,
       isFetching: false,
       fetchError: true,
       errorMessage: errors,
 
     }),
-    SUCCESS: state => state.mergeIn(['authenticated'], {
+    SUCCESS: state => state.merge({
       fetched: true,
       isFetching: false,
       fetchError: false,
     }),
-  }
+  },
 }, initialState);
 
 export default reducer;
