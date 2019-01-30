a8167da2cb52cfeb9058be1b3d62b9572ee73851
wilmer <wilmer.payall@gmail.com>
2019-01-30 20:13:21 -0100

    Change State Transform

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
+import Immutable from 'immutable';
+import _ from 'lodash';
 import createSagaMiddleware from 'redux-saga';
 import { createLogger } from 'redux-logger';
 import rootReducer from './rootReducer';
@@ -7,14 +9,28 @@ const sagaMiddleware = createSagaMiddleware();
 
 let middlewares = [sagaMiddleware];
 
-/*if (process.env.NODE_ENV === 'development') {
+const setStateTransformer = (state) => {
+  const newState = {};
+
+  _.each(state, (value, key) => {
+    if (Immutable.isImmutable(value)) {
+      newState[key] = value.toJS();
+    } else {
+      newState[key] = setStateTransformer(value);
+    }
+  });
+
+  return newState;
+}
+
+if (process.env.NODE_ENV === 'development') {
   const logger = createLogger({
     collapse: true,
-    stateTransformer: state => state.toJS(),
+    stateTransformer: state =>  setStateTransformer(state),
   });
 
   middlewares = [...middlewares, logger];
-}*/
+}
 
 const store = createStore(
   rootReducer,
