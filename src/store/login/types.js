c5a4e6f75988922414261685e1886d0d649abce9
wilmer <wilmer.payall@gmail.com>
2019-01-30 19:30:01 -0100

    Create Login Component

---
+++ src/store/login/types.js
@@ -0,0 +1,8 @@
+/**
+* Login Types
+**/
+import { createAsyncTypes } from '../Utilities';
+
+export const LOGIN_USER_ASYNC = createAsyncTypes('LOGIN_USER');
+export const OPEN_LOGIN_MODAL = 'OPEN_LOGIN_MODAL';
+export const CLOSE_LOGIN_MODAL = 'CLOSE_LOGIN_MODAL';
