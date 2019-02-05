7191c0744143f679fc0212b068fcf9415e4c49ca
wilmer <wilmer.payall@gmail.com>
2019-02-04 23:48:56 -0100

    Validate not found response

--- src/utils/BookClient.js
+++ src/utils/BookClient.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
-import { isEqual } from 'lodash';
-import { UNPROCESSABLE_ENTITY, SERVER_ERRORS } from './statusCodes';
-import { serverErrorMessage, timeoutMessage, requestFailedMessage } from './messages';
+import _ from 'lodash';
+import { fromJS } from 'immutable';
+import { UNPROCESSABLE_ENTITY, SERVER_ERRORS, NOT_FOUND } from './statusCodes';
+import { 
+  serverErrorMessage, 
+  timeoutMessage, 
+  requestFailedMessage,
+  notFoundMessage 
+} from './messages';
 
 const BASE_URL = 'http://book.com/v1/';
 const TIMEOUT = 5000;
 const CONTENT_TYPE = 'application/x-www-form-urlencoded';
-const Econnaborted = 'ECONNABORTED';
+const ECONNABORTED = 'ECONNABORTED';
 
 
 const axiosClient = axios.create({
@@ -18,47 +24,51 @@ const axiosClient = axios.create({
   },
 });
 
-axiosClient.defaults.headers.post['Content-type'] = CONTENT_TYPE;
-axiosClient.defaults.headers.put['Content-type'] = CONTENT_TYPE;
-axiosClient.defaults.headers.patch['Content-type'] = CONTENT_TYPE;
+
+const passToken = token => axiosClient.defaults.headers.common['Authorization'] = `Bearer token`;
+const removeToken = () => axiosClient.defaults.headers.common['Authorization'] = ''; 
 
 
-const handleErrorResponse = (code, { status, data: { message, errors } }) => {
+const handleError = (code, { status, data: { message, errors } }) => {
   let errorResponse = message;
 
   if (code === ECONNABORTED) {
     errorResponse = timeoutMessage;
-  } else if(isEqual(status, UNPROCESSABLE_ENTITY)) {
+  } else if(_.isEqual(status, UNPROCESSABLE_ENTITY)) {
     errorResponse = fromJS(errors);
   } else if (status >= SERVER_ERRORS) {
     errorResponse = serverErrorMessage;
+  } else if (_.isEqual(status, NOT_FOUND)) {
+    errorResponse = notFoundMessage;
   }
 
   return errorResponse;
-};
+}
 
-const handleError = ({ response, request, code }) => {
+axiosClient.defaults.headers.post['Content-type'] = CONTENT_TYPE;
+axiosClient.defaults.headers.put['Content-type'] = CONTENT_TYPE;
+axiosClient.defaults.headers.patch['Content-type'] = CONTENT_TYPE;
+
+axiosClient.interceptors.response.use(response => response, (error) => {
+  const { response, request, code } = error;
   let errors;
 
   if (response) {
-    errors = handleErrorStatus(code, response);
+    errors = handleError(code, response);
   } else if (request) {
     errors = requestFailedMessage;
   }
 
   return Promise.reject({ errors });
-};
-
-const passToken = token => axiosClient.defaults.headers.common['Authorization'] = `Bearer token`;
-const removeToken = () => axiosClient.defaults.headers.common['Authorization'] = ''; 
+});
 
 const get = (...args) => {
   return new Promise((resolve, reject) => {
-    axiosClient.get(..args)
+    axiosClient.get(...args)
       .then(response => resolve(response))
       .catch(err => reject(err));
   });
-};
+}
 
 const post = (...args) => {
   return new Promise((resolve, reject) => {
@@ -66,7 +76,7 @@ const post = (...args) => {
       .then(response => resolve(response))
       .catch(err => reject(err));
   });
-};
+}
 
 const bookClient = {
   get,
