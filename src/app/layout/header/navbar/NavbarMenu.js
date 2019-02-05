934c6b5551aa9995a4936f291cf21e0a4857bc81
wilmer <wilmer.payall@gmail.com>
2019-02-05 00:51:44 -0100

    Move SearchFiel Component one branch up

--- src/app/layout/header/navbar/NavbarMenu.js
+++ src/app/layout/header/navbar/NavbarMenu.js
@@ -2,8 +2,8 @@ import React, { PureComponent, Fragment } from 'react';
 import ImmutableProtypes from 'react-immutable-proptypes';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import SearchField from './navbarMenu/SearchField';
-import Avatar from './navbarMenu/Avatar';
+import SearchField from './SearchField';
+import Avatar from './Avatar';
 import Modal from '../../../components/Modal';
 import Login from '../../../Login';
 
