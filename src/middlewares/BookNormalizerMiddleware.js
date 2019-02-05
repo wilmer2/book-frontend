8a3dc813141ac12cd7f6ff96d879cd0322d9cd18
wilmer <wilmer.payall@gmail.com>
2019-02-05 03:06:05 -0100

    Add middleware to normalize data response

---
+++ src/middlewares/BookNormalizerMiddleware.js
@@ -0,0 +1,15 @@
+import normalize from 'json-api-normalizer';
+import _ from 'lodash';
+import { mergeEntities } from '../store/Entities';
+
+const middleware = ({ dispatch }) => next => (action) => {
+  if (!_.has(action, ['payload', 'data'])) {
+    return next(action);
+  }
+
+  const normalizeData = normalize(action.payload);
+
+  dispatch(mergeEntities(normalizeData));
+}
+
+export default middleware;
