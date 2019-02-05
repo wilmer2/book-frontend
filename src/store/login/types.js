0b36764bb3f70d667f017993461f4b8f2ffae039
wilmer <wilmer.payall@gmail.com>
2019-02-04 23:50:24 -0100

    Add LOGOUT action types

--- src/store/login/types.js
+++ src/store/login/types.js
@@ -6,3 +6,4 @@ import { createAsyncTypes } from '../Utilities';
 export const LOGIN_USER_ASYNC = createAsyncTypes('LOGIN_USER');
 export const OPEN_LOGIN_MODAL = 'OPEN_LOGIN_MODAL';
 export const CLOSE_LOGIN_MODAL = 'CLOSE_LOGIN_MODAL';
+export const LOGOUT = 'LOGOUT';
