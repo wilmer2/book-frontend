1a636b8fcdbb083b370ee30057556e922d293127
wilmer <wilmer.payall@gmail.com>
2019-02-10 21:48:04 -0100

    Return initial state when LOGOUT Action is dispatched

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,15 +2,23 @@ import { combineReducers } from 'redux';
 import entities from './entities/reducer';
 import authenticated from './authenticated/reducer';
 import login from './login/reducer';
+import isEqual from 'lodash/isEqual';
+import { LOGOUT } from './login/types';
 
-
-
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
   entities,
   ui: combineReducers({
     authenticated,
     login,
   }), 
 });
+
+const rootReducer = (state, action) => {
+  if (isEqual(action.type, LOGOUT)) {
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+}
 
 export default rootReducer;
