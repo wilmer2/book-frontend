5c530e741ae59075d96815cff40058eff8466ee7
wilmer <wilmer.payall@gmail.com>
2019-02-10 21:57:43 -0100

    Add id attribute to initialState

--- src/store/authenticated/reducer.js
+++ src/store/authenticated/reducer.js
@@ -5,33 +5,33 @@ import typeToReducer from 'type-to-reducer';
 import { fromJS } from 'immutable'; 
 import { GET_AUTHENTICATED_USER_ASYNC } from './types';
 
+
 const initialState = fromJS({
-  isFetching: true,
+  isFetching: false,
   fetched: false,
   fetchError: false,
-  errorMessage: '',
+  id: null,
 });
 
 const reducer = typeToReducer({
-  [GET_AUTHENTICATED_USER_ASYNC]: {
-    PENDING: state => state.merge({
-      fetched: false,
-      isFetching: true,
-      fetchError: false,
-    }),
-    ERROR: (state, { payload: { errors } }) => state.merge({
-      fetched: false,
-      isFetching: false,
-      fetchError: true,
-      errorMessage: errors,
-
-    }),
-    SUCCESS: state => state.merge({
+  [GET_AUTHENTICATED_USER_ASYNC.PENDING]: state => state.merge({
+    fetched: false,
+    isFetching: true,
+    fetchError: false,
+  }),
+  [GET_AUTHENTICATED_USER_ASYNC.ERROR]: state => state.merge({
+    fetched: false,
+    isFetching: false,
+    fetchError: true,
+  }),
+  [GET_AUTHENTICATED_USER_ASYNC.SUCCESS]: (state, { payload: { data: { id } }}) => {
+    return state.merge({
       fetched: true,
       isFetching: false,
       fetchError: false,
-    }),
-  },
+      id,
+    });
+  }, 
 }, initialState);
 
 export default reducer;
