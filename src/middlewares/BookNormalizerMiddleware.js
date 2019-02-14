7e905dce3718102d5705ba1605d7df8c2ab78a31
wilmer <wilmer.payall@gmail.com>
2019-02-14 19:55:55 -0100

    Customize pagination data

--- src/middlewares/BookNormalizerMiddleware.js
+++ src/middlewares/BookNormalizerMiddleware.js
@@ -7,10 +7,39 @@ const middleware = ({ dispatch }) => next => (action) => {
     return next(action);
   }
 
-  const normalizeData = normalize(action.payload);
+  let payload = action.payload;
+  let normalizeData;
+  const nextPayload = {};
+
+  if (_.has(payload, 'endpoint')) {
+    const endpoint = payload.endpoint;
+    payload = _.omit(payload, ['endpoint']);
+
+    normalizeData = normalize(payload, { endpoint, filterEndpoint: false });
+  } else {
+    normalizeData = normalize(payload);
+  }
+
+  if (_.hasIn(payload, ['meta', 'pagination'])) {
+    const pagination = _.omit(payload, ['include', 'custom']);;
+
+    nextPayload.pagination = {
+      totalPages: pagination.total_pages,
+      currentPage: pagination.current_page,
+    };
+  }
+
+  if (_.isArray(payload.data)) {
+    _.merge(nextPayload, { idsList: payload.data.map(data => data.id) });
+  } else {
+    _.merge(nextPayload, { id: payload.data.id });
+  }
+
+  _.merge(action.payload, nextPayload);
+
 
   dispatch(mergeEntities(normalizeData));
-  next(action);
+  return next(action);
 }
 
 export default middleware;
