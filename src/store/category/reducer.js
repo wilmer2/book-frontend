e42dc1acafdfce965ae23cd9422a21b77c58214c
Wilmerq <wilmer.payall@gmail.com>
2019-03-05 23:02:02 -0400

    Add category reducer

---
+++ src/store/category/reducer.js
@@ -0,0 +1,33 @@
+/**
+* Category Reducer
+*/
+import typeToReducer from 'type-to-reducer';
+import { fromJS } from 'immutable';
+import { GET_CATEGORIES_ASYNC } from './types';
+
+const initialState = fromJS({
+  isFetching: true,
+  fetched: false,
+  fetchError: false,
+  idsList: null,
+});
+
+const reducer = typeToReducer({
+  [GET_CATEGORIES_ASYNC.PENDING]: state => state.merge({
+    fetched: false,
+    fetchError: false,
+    isFetching: true,
+  }),
+  [GET_CATEGORIES_ASYNC.ERROR]: state => state.merge({
+    fetchError: true,
+    isFetching: false,
+  }),
+  [GET_CATEGORIES_ASYNC.SUCCESS]: (state, { payload: { idsList } }) => state.merge({
+    fetched: true,
+    fetchError: false,
+    isFetching: false,
+    idsList,
+  }), 
+}, initialState);
+
+export default reducer;
