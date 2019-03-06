c962b5698187febccbd769b4b525a0eac25b0d47
Wilmerq <wilmer.payall@gmail.com>
2019-03-05 23:10:25 -0400

    Delete files

--- src/store/book/rootSaga.js
+++
@@ -1,6 +0,0 @@
-import { fork } from 'redux-saga/effects';
-import { getBooksToHomeSaga } from './sagas';
-
-export default function* rootSaga() {
-  yield fork(getBooksToHomeSaga);
-}
--- src/store/category/rootSaga.js
+++
@@ -1,6 +0,0 @@
-import { fork } from 'redux-saga/effects';
-import getCategoriesSaga from './sagas/getCategoriesSaga';
-
-export default function* rootSaga() {
-  yield fork(getCategoriesSaga);
-}
