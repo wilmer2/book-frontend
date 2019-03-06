657df0b96503ab610624bcc7c0b8caba279a22f9
Wilmerq <wilmer.payall@gmail.com>
2019-03-05 23:10:09 -0400

    Add Carousel

--- src/app/pages/home/HomeView.js
+++ src/app/pages/home/HomeView.js
@@ -1,46 +1,63 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import isNull from 'lodash/isNull';
+import isEqual from 'lodash/isEqual';
+import isEmpty from 'lodash/isEmpty';
 import MediumSpinner from '../../components/MediumSpinner';
 import ButtonReload from '../../components/ButtonReload';
+import CarouselItem from './CarouselItem';
 
 class HomeView extends PureComponent {
-  /*componentDidMount() {
-    const token = localStorage.getItem('token');
+  componentDidMount() {
+    this.handleGetBooks();
+  }
 
-    if (!token) {
+  componentDidUpdate(prevProps) {
+    if (!isEqual(prevProps.categoriesIds, this.props.categoriesIds)) {
       this.handleGetBooks();
     }
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.authFetched !== this.props.authFetched && this.props.authFetched) {
-      this.handleGetBooks();
-    }
-  }*/
-
   handleGetBooks = () => {
+    if (isNull(this.props.categoriesIds)) {
+      this.props.getCategories();
+      return;
+    }
+
     this.props.getBooks({
       lastSearch: this.props.lastSearch,
       categoriesIds: this.props.categoriesIds,
     });
   }
 
+  renderCarouselByLastSearch() {
+    return (!isEmpty(this.props.lastSearch) ? 
+      <CarouselItem title="Por última búsqueda" books={this.props.booksByLastSearch} /> : null
+    );
+  }
+
   render() {
+    if (this.props.isFetching && !this.props.fetchError) {
+      return (
+        <MediumSpinner />
+      );
+    }
+
     if (this.props.fetched) {
-      //
+      return (
+        <Fragment>
+          <CarouselItem title="Más vistos" books={this.props.booksMoreSeen} />
+          <CarouselItem title="Recomendaciones" books={this.props.booksByCategories} />
+          {this.renderCarouselByLastSearch()}
+        </Fragment>
+      );
     }
 
     if (this.props.fetchError) {
       return (
         <ButtonReload onClickFunc={this.handleGetBooks} />
       );
-    }
-
-    if (this.props.isFetching) {
-      return (
-        <MediumSpinner />
-      );
-    }
+    }    
   }
 }
 
@@ -50,10 +67,18 @@ HomeView.propTypes = {
   fetchError: PropTypes.bool.isRequired,
   lastSearch: PropTypes.string,
   categoriesIds: PropTypes.array,
-  authFetched: PropTypes.bool.isRequired,
-  booksMoreSeen: PropTypes.object.isRequired,
-  booksByCategories: PropTypes.object.isRequired,
-  booksByLastSearch: PropTypes.object.isRequired,
+  booksMoreSeen: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array
+  ]).isRequired,
+  booksByCategories: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array
+  ]).isRequired,
+  booksByLastSearch: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array
+  ]).isRequired,
   getBooks: PropTypes.func.isRequired,
 };
 
