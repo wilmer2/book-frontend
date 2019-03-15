6293941f69a165ac3ce711244a5be1d6d56b6dab
Wilmerq <wilmer.payall@gmail.com>
2019-03-15 15:54:19 -0400

    Add book modal reducers

--- src/store/book/reducers/booksToHomeReducer.js
+++ src/store/book/reducers/booksToHomeReducer.js
@@ -5,7 +5,9 @@ import {
   GET_BOOKS_TO_HOME_ASYNC,
   PUT_BOOKS_IDS_MORE_SEEN, 
   PUT_BOOKS_IDS_BY_CATEGORIES, 
-  PUT_BOOKS_IDS_BY_LAST_SEARCH 
+  PUT_BOOKS_IDS_BY_LAST_SEARCH ,
+  OPEN_BOOK_MODAL,
+  CLOSE_BOOK_MODAL,
 } from '../types';
 
 const booksToHomeReducer = {
@@ -32,6 +34,11 @@ const booksToHomeReducer = {
   [PUT_BOOKS_IDS_MORE_SEEN]: (state, { payload: { idsList } }) => state.setIn(['homeData', 'booksIdsMoreSeen'], idsList),
   [PUT_BOOKS_IDS_BY_CATEGORIES]: (state, { payload: { idsList } }) => state.setIn(['homeData', 'booksIdsByCategories'], idsList),
   [PUT_BOOKS_IDS_BY_LAST_SEARCH]: (state, { payload: { idsList } }) => state.setIn(['homeData', 'booksIdsByLastSearch'], idsList),
+  [OPEN_BOOK_MODAL]: (state, { payload: { bookId } }) => state.withMutations((mutator) => {
+    mutator.setIn(['homeData', 'bookIdModal'], bookId);
+    mutator.setIn(['homeData', 'openModal'], true);
+  }),
+  [CLOSE_BOOK_MODAL]: state => state.setIn(['homeData', 'openModal'], false),
 };
 
 export default booksToHomeReducer;
