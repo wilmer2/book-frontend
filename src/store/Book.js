5c80e64fb00b1dbfc863de3828552cbca001e108
Wilmerq <wilmer.payall@gmail.com>
2019-03-15 15:55:43 -0400

    Add book modal action creators

--- src/store/Book.js
+++ src/store/Book.js
@@ -5,8 +5,10 @@ import { createAction } from 'redux-actions';
 import { 
   GET_BOOKS_TO_HOME_ASYNC, 
   PUT_BOOKS_IDS_MORE_SEEN, 
-  PUT_BOOKS_IDS_BY_CATEGORIES , 
-  PUT_BOOKS_IDS_BY_LAST_SEARCH 
+  PUT_BOOKS_IDS_BY_CATEGORIES, 
+  PUT_BOOKS_IDS_BY_LAST_SEARCH,
+  OPEN_BOOK_MODAL,
+  CLOSE_BOOK_MODAL,
 } from './book/types';
 
 export const getBooksToHomePending = createAction(GET_BOOKS_TO_HOME_ASYNC.PENDING);
@@ -15,3 +17,5 @@ export const getBooksToHomeSuccess = createAction(GET_BOOKS_TO_HOME_ASYNC.SUCCES
 export const putBooksMoreSeend = createAction(PUT_BOOKS_IDS_MORE_SEEN);
 export const putBooksByCategories = createAction(PUT_BOOKS_IDS_BY_CATEGORIES);
 export const putBooksByLastSearch = createAction(PUT_BOOKS_IDS_BY_LAST_SEARCH);
+export const openBookModal = createAction(OPEN_BOOK_MODAL);
+export const closeBookModal = createAction(CLOSE_BOOK_MODAL);
