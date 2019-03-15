2b24db904adee7ce4f755a0128b340bf8fa55e3b
Wilmerq <wilmer.payall@gmail.com>
2019-03-15 16:46:48 -0400

    Change categories selector

--- src/selectors/categoriesSelector.js
+++ src/selectors/categoriesSelector.js
@@ -4,8 +4,5 @@
 import createDenormalizeSelector from './createDenormalizeSelector';
 
 const getCategoriesIds = state => state.ui.category.get('idsList');
-const getCategories = state => createDenormalizeSelector(getCategoriesIds, 'categories', state.entities)(state);
 
-export default {
-  getCategories,
-};
+export const getCategoriesSelector = state => createDenormalizeSelector(getCategoriesIds, 'categories', state.entities)(state);
