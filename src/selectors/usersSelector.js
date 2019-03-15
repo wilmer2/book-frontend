aef21d89aa4d0a53eb5dff993c607b902baa73c3
Wilmerq <wilmer.payall@gmail.com>
2019-03-15 16:39:03 -0400

    Change user selectors

--- src/selectors/usersSelector.js
+++ src/selectors/usersSelector.js
@@ -4,8 +4,5 @@
 import createDenormalizeSelector from './createDenormalizeSelector';
 
 const getAuthenticatedId = state => state.ui.authenticated.get('id');
-const getAuthenticatedUser = state => createDenormalizeSelector(getAuthenticatedId, 'authenticated', state.entities)(state); 
 
-export default {
-  getAuthenticatedUser,
-};
+export const getAuthenticatedUserSelector = state => createDenormalizeSelector(getAuthenticatedId, 'authenticated', state.entities)(state); 
