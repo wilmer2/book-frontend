9d69740742fb516bb6de40c0a8fa4e00f4fd3fa3
Wilmerq <wilmer.payall@gmail.com>
2019-04-21 19:49:20 -0400

    return component directly

--- src/app/layout/Router.js
+++ src/app/layout/Router.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import BookStory from '../pages/BookStory';
+import Home from '@/app/views/Home';
+import Page from '@/app/views/Page/Show';
 
-
-const Router = () => {
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exac path="/book-story/:bookId/:pageId" component={BookStory} />
-    </Switch>
-  );
-}
+const Router = () => <Switch>
+  <Route exact path="/" component={Home} />
+  <Route exac path="/book-story/:bookId/:pageId" component={Page} />
+</Switch>;
 
 export default Router;
