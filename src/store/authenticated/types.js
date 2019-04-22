8748dc8e084b91c54cba6f3200cae227f169c1f6
Wilmerq <wilmer.payall@gmail.com>
2019-04-21 20:24:48 -0400

    import utils

--- src/store/authenticated/types.js
+++ src/store/authenticated/types.js
@@ -1,6 +1,3 @@
-/**
-* Authenticated Types
-*/
-import { createAsyncTypes } from '../Utilities';
+import { createAsyncTypes } from '@/store/utils';
 
 export const GET_AUTHENTICATED_USER_ASYNC = createAsyncTypes('GET_AUTHENTICATED_USER');
