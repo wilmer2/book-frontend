279c8f964039f319d7cc13994f8ccd43da662240
Wilmerq <wilmer.payall@gmail.com>
2019-04-21 20:33:04 -0400

    add createResolver func and renaming idsList by id

--- src/store/category/reducer.js
+++ src/store/category/reducer.js
@@ -1,33 +1,22 @@
-/**
-* Category Reducer
-*/
 import typeToReducer from 'type-to-reducer';
 import { fromJS } from 'immutable';
-import { GET_CATEGORIES_ASYNC } from './types';
+import { GET_CATEGORIES_ASYNC, PUT_CATEGORIES_IDS } from './types';
+import { createResolver } from '@/store/utils';
+
+const categoryResolver = createResolver();
 
 const initialState = fromJS({
   isFetching: true,
   fetched: false,
   fetchError: false,
-  idsList: null,
+  ids: [],
 });
 
 const reducer = typeToReducer({
-  [GET_CATEGORIES_ASYNC.PENDING]: state => state.merge({
-    fetched: false,
-    fetchError: false,
-    isFetching: true,
-  }),
-  [GET_CATEGORIES_ASYNC.ERROR]: state => state.merge({
-    fetchError: true,
-    isFetching: false,
-  }),
-  [GET_CATEGORIES_ASYNC.SUCCESS]: (state, { payload: { idsList } }) => state.merge({
-    fetched: true,
-    fetchError: false,
-    isFetching: false,
-    idsList,
-  }), 
+  [GET_CATEGORIES_ASYNC.SUCCESS]: (state, { payload }) => categoryResolver.success(state, payload),
+  [GET_CATEGORIES_ASYNC.PENDING]: state => categoryResolver.pending(state),
+  [GET_CATEGORIES_ASYNC.ERROR]: state => categoryResolver.error(state),
+  [PUT_CATEGORIES_IDS]: (state, { payload: { ids } }) => state.set('ids', ids),
 }, initialState);
 
 export default reducer;
