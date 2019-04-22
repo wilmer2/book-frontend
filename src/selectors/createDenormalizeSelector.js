29b5970576741a1c9e024d17bc85934ff9afc124
Wilmerq <wilmer.payall@gmail.com>
2019-04-21 20:17:08 -0400

    separte selector creator by list and by id

--- src/selectors/createDenormalizeSelector.js
+++ src/selectors/createDenormalizeSelector.js
@@ -1,34 +1,23 @@
 import { createSelectorCreator, defaultMemoize } from 'reselect';
-import { is, List } from 'immutable';
+import { is } from 'immutable';
 import build from 'redux-object';
-import isArray from 'lodash/isArray';
-import isUndefined from 'lodash/isUndefined';
-import isNull from 'lodash/isNull';
 
 export const createImmutableSelector = createSelectorCreator(defaultMemoize, is);
-
-const getEntities = state => state.entities;
-
-const getEntityFactory = entityName => createImmutableSelector(
-  getEntities, 
-  entities => entities.get(entityName)
+    
+export const createDenormalizeSelectorById = (
+  getIdFunc,
+  getEntitiesFunc,
+  entityName,
+  rootEntities
+) => createImmutableSelector([getIdFunc, getEntitiesFunc],
+  (id, _) => id ? build(rootEntities, entityName, id) : {}
 );
 
-const createDenormalizeSelector = (getIdsFunc, entityName, rootEntities) => {
-  return createImmutableSelector([getIdsFunc, getEntityFactory(entityName)], 
-    (idsList, _) => {
-      if (isArray(idsList) || idsList instanceof List) {
-        return idsList.map(id => build(rootEntities, entityName, id));
-      }
-
-      const id = idsList;
-
-      if (isNull(id) || isUndefined(id)) {
-        return {};
-      }
-
-      return build(rootEntities, entityName, id);
-  });
-}
-
-export default createDenormalizeSelector;
+export const createDenormalizeSelectorByList = (
+  getIdsFunc,
+  getEntitiesFunc,
+  entityName,
+  rootEntities
+) => createImmutableSelector([getIdsFunc, getEntitiesFunc],
+  (ids, _) => ids.map(id => build(rootEntities, entityName, id))
+);
