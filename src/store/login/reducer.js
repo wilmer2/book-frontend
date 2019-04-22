fcf802b5dd272b3683e499a1d22e2aeb94334efe
Wilmerq <wilmer.payall@gmail.com>
2019-04-21 20:41:42 -0400

    add createResolver func

--- src/store/login/reducer.js
+++ src/store/login/reducer.js
@@ -1,9 +1,9 @@
-/*
-* Login Reducer
-*/
 import typeToReducer from 'type-to-reducer';
 import { fromJS } from 'immutable';
 import { LOGIN_USER_ASYNC, OPEN_LOGIN_MODAL, CLOSE_LOGIN_MODAL } from './types';
+import { createResolver } from '@/store/utils';
+
+const loginResolver = createResolver();
 
 const initialState = fromJS({
   isFetching: false,
@@ -13,24 +13,14 @@ const initialState = fromJS({
 });
 
 const reducer = typeToReducer({
-  [LOGIN_USER_ASYNC.PENDING]: state => state.merge({
-    isFetching: true,
-    fetchError: false,
-  }),
-  [LOGIN_USER_ASYNC.ERROR]: (state, { payload: { errors } }) => state.merge({
-    isFetching: false,
-    fetchError: true,
-    errorMessage: errors,
-  }),
-  [LOGIN_USER_ASYNC.SUCCESS]: state => state.merge({
-    isFetching: false,
-    fetchError: false,
-  }),
-  [OPEN_LOGIN_MODAL]: (state) => state.set('openModal', true),
-  [CLOSE_LOGIN_MODAL]: (state) => state.merge({
+  [LOGIN_USER_ASYNC.SUCCESS]: state => loginResolver.success(state),
+  [LOGIN_USER_ASYNC.PENDING]: state => loginResolver.pending(state),
+  [LOGIN_USER_ASYNC.ERROR]: (state, { payload }) => loginResolver.error(state, payload),
+  [OPEN_LOGIN_MODAL]: state => state.set('openModal', true),
+  [CLOSE_LOGIN_MODAL]: state => state.merge({
     openModal: false,
     fetchError: false,
-  })
+  }),
 }, initialState);
 
 export default reducer;
