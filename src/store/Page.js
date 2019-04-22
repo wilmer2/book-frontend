300e9ec36d20568aa495279c55e344da1e23eb4e
Wilmerq <wilmer.payall@gmail.com>
2019-04-21 20:20:17 -0400

    removed requestPagePagination action

--- src/store/Page.js
+++ src/store/Page.js
@@ -2,11 +2,7 @@
 * Page Actions
 */
 import { createAction } from 'redux-actions';
-import { 
-  GET_PAGES_ASYNC, 
-  GET_PAGE_BY_ID_ASYNC, 
-  REQUEST_PAGINATION_OF_PAGES 
-} from './page/types';
+import { GET_PAGES_ASYNC, GET_PAGE_BY_ID_ASYNC } from './page/types';
 
 export const getPagesSuccess = createAction(GET_PAGES_ASYNC.SUCCESS);
 export const getPagesError = createAction(GET_PAGES_ASYNC.ERROR);
@@ -14,4 +10,3 @@ export const getPagesPending = createAction(GET_PAGES_ASYNC.PENDING);
 export const getPageByIdSuccess = createAction(GET_PAGE_BY_ID_ASYNC.SUCCESS);
 export const getPageByIdError = createAction(GET_PAGE_BY_ID_ASYNC.ERROR);
 export const getPageByIdPending = createAction(GET_PAGE_BY_ID_ASYNC.PENDING);
-export const requestPaginationPages = createAction(REQUEST_PAGINATION_OF_PAGES);
