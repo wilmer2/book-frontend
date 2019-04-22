5af0bba33e6d0095feffe90a49f65713a9b2c3c7
Wilmerq <wilmer.payall@gmail.com>
2019-04-21 20:42:17 -0400

    import sagas per file

--- src/store/login/sagas.js
+++ src/store/login/sagas.js
@@ -1,6 +1,6 @@
 import { fork } from 'redux-saga/effects';
-import loginSaga from './loginSaga';
-import logoutSaga from './logoutSaga';
+import loginSaga from './sagas/loginSaga';
+import logoutSaga from './sagas/logoutSaga';
 
 export default function* root() {
   yield fork(loginSaga);
