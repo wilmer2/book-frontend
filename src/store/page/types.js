515dc74b6b4c5160576d9ac44f177d3bbd664197
Wilmerq <wilmer.payall@gmail.com>
2019-04-21 20:45:32 -0400

    removed REQUEST_PAGINATION_OF_PAGES action type

--- src/store/page/types.js
+++ src/store/page/types.js
@@ -1,8 +1,4 @@
-/**
-* Page Types
-*/
-import { createAsyncTypes } from '../Utilities';
+import { createAsyncTypes } from '@/store/utils';
 
 export const GET_PAGES_ASYNC = createAsyncTypes('GET_PAGES');
 export const GET_PAGE_BY_ID_ASYNC = createAsyncTypes('GET_PAGE_BY_ID');
-export const REQUEST_PAGINATION_OF_PAGES = 'REQUEST_PAGINATION_OF_PAGES';
