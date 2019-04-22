032e284575cafb19d8232b233febe2fc2503a628
Wilmerq <wilmer.payall@gmail.com>
2019-04-21 20:36:34 -0400

    add PUT_CATEGORIES_IDS actions type

--- src/store/category/types.js
+++ src/store/category/types.js
@@ -1,6 +1,4 @@
-/**
-* Category Types
-*/
-import { createAsyncTypes } from '../Utilities';
+import { createAsyncTypes } from '@/store/utils';
 
 export const GET_CATEGORIES_ASYNC = createAsyncTypes('GET_CATEGORIES');
+export const PUT_CATEGORIES_IDS = 'PUT_CATEGORIES_IDS';
