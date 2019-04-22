b58567e2279192fd7fb9b31ef7a2f8e56dfa52d6
Wilmerq <wilmer.payall@gmail.com>
2019-04-21 20:43:44 -0400

    add createResolver func

--- src/store/page/reducer.js
+++ src/store/page/reducer.js
@@ -1,9 +1,10 @@
-/**
-* Page Reducer
-*/
 import typeToReducer from 'type-to-reducer';
 import { fromJS } from 'immutable';
-import { pagesPaginationReducer, pageByIdReducer }  from './reducers';
+import { GET_PAGE_BY_ID_ASYNC, GET_PAGES_ASYNC } from './types';
+import { createResolver } from '@/store/utils';
+
+const resolverById = createResolver('byId');
+const pageResolver = createResolver();
 
 const initialState = fromJS({
   isFetching: true,
@@ -12,6 +13,7 @@ const initialState = fromJS({
   pagination: {
     totalPages: 1,
     currentPage: 1,
+    ids: null,
   },
   byId: {
     fetched: false,
@@ -22,8 +24,13 @@ const initialState = fromJS({
 });
 
 const reducer = typeToReducer({
-  ...pagesPaginationReducer,
-  ...pageByIdReducer,
+  [GET_PAGE_BY_ID_ASYNC.SUCCESS]: (state, { payload }) => resolverById.success(state, payload),
+  [GET_PAGE_BY_ID_ASYNC.PENDING]: state => resolverById.pending(state),
+  [GET_PAGE_BY_ID_ASYNC.ERROR]: (state, { payload }) => resolverById.error(state, payload),
+  [GET_PAGES_ASYNC.SUCCESS]: (state, { payload }) => pageResolver.success(state, payload),
+  [GET_PAGES_ASYNC.PENDING]: state => pageResolver.pending(state),
+  [GET_PAGES_ASYNC.ERROR]: (state, { payload }) =>  pageResolver.error(state, payload),
+
 }, initialState);
 
 export default reducer;
