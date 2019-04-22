92d9d5685b799c61180bf0826145a50ccefaa96e
Wilmerq <wilmer.payall@gmail.com>
2019-04-21 20:28:23 -0400

    import from alias

--- src/store/book/reducer.js
+++ src/store/book/reducer.js
@@ -1,9 +1,17 @@
-/**
-* Book Reducer
-*/
 import typeToReducer from 'type-to-reducer';
 import { fromJS } from 'immutable';
-import { booksToHomeReducer } from './reducers';
+import { createResolver } from '@/store/utils';
+
+import { 
+  GET_BOOKS_TO_HOME_ASYNC,
+  PUT_BOOKS_IDS_MORE_SEEN, 
+  PUT_BOOKS_IDS_BY_CATEGORIES, 
+  PUT_BOOKS_IDS_BY_LAST_SEARCH ,
+  OPEN_BOOK_MODAL,
+  CLOSE_BOOK_MODAL
+} from './types';
+
+const homeDataResolver = createResolver('homeData');
 
 const initialState = fromJS({
   byUser: {
@@ -24,16 +32,29 @@ const initialState = fromJS({
     fetched: false,
     fetchError: false,
     lastSearch: '',
-    booksIdsMoreSeen: null,
-    booksIdsByCategories: null,
-    booksIdsByLastSearch: null,
+    booksIdsMoreSeen: [],
+    booksIdsByCategories: [],
+    booksIdsByLastSearch: [],
     bookIdModal: null,
     openModal: false,
   },
 });
 
 const reducer = typeToReducer({
-  ...booksToHomeReducer,
+  [GET_BOOKS_TO_HOME_ASYNC.SUCCESS]: state => homeDataResolver.success(state),
+  [GET_BOOKS_TO_HOME_ASYNC.PENDING]: state => homeDataResolver.pending(state), 
+  [GET_BOOKS_TO_HOME_ASYNC.ERROR]: state => homeDataResolver.error(state),
+  [PUT_BOOKS_IDS_MORE_SEEN]: (state, { payload: { ids } }) => 
+    state.setIn(['homeData', 'booksIdsMoreSeen'], ids),
+  [PUT_BOOKS_IDS_BY_CATEGORIES]: (state, { payload: { ids } }) => 
+    state.setIn(['homeData', 'booksIdsByCategories'], ids),
+  [PUT_BOOKS_IDS_BY_LAST_SEARCH]: (state, { payload: { ids } }) => 
+    state.setIn(['homeData', 'booksIdsByLastSearch'], ids),
+  [OPEN_BOOK_MODAL]: (state, { payload: { bookId } }) => state.withMutations(mutator => {
+    mutator.setIn(['homeData', 'bookIdModal'], bookId);
+    mutator.setIn(['homeData', 'openModal'], true);
+  }),
+  [CLOSE_BOOK_MODAL]: state => state.setIn(['homeData', 'openModal'], false),
 }, initialState);
 
 export default reducer;
