3d0e50d7f3a5ec88007011c64910303b5074903c
Wilmerq <wilmer.payall@gmail.com>
2019-04-29 15:59:52 -0400

    Add toastr

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store';
 import App from './App';
@@ -12,6 +13,16 @@ class AppContainer extends Component {
         <Router>
           <App />
         </Router>
+        <ReduxToastr
+          timeOut={4000}
+          newestOnTop={false}
+          preventDuplicates
+          position="top-left"
+          transitionIn="fadeIn"
+          transitionOut="fadeOut"
+          progressBar
+          closeOnToastrClick
+        />
       </Provider>
     );
   }
