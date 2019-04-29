4705bd43d2bf94e17a4752b9912b6fc773acec97
Wilmerq <wilmer.payall@gmail.com>
2019-04-29 15:50:39 -0400

    Fixed error server message

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,4 +1,4 @@
-export const serverErrorMessage = 'Ha ocurrio un problemea con el servidor, vuelve a intentarlo';
+export const serverErrorMessage = 'Ha ocurrio un problema con el servidor, vuelve a intentarlo';
 export const timeoutMessage = 'No se ha podido establecer conexión con el servidor';
 export const requestFailedMessage = 'Error al establecer conexión con el servidor, asegurate de tener acceso a internet';
 export const notFoundMessage = 'El recurso no ha sido encontrado';
