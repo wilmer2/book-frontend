249a8241b5489f514904189889f33fb611c85241
Wilmerq <wilmer.payall@gmail.com>
2019-05-10 22:20:06 -0400

    parse form validation errors

--- src/store/login/sagas/loginSaga.js
+++ src/store/login/sagas/loginSaga.js
@@ -1,27 +1,40 @@
 import { fork, call, take, put, cancelled, cancel } from 'redux-saga/effects';
 import isEqual from 'lodash/isEqual';
+import isObject from 'lodash/isObject';
 import { LOGIN_USER_ASYNC, LOGOUT } from '@/store/login/types';
 import { loginError, loginSuccess, closeLoginModal } from '@/store/Login';
 import { getAuthenticatedUserPending } from '@/store/Authenticated';
 import BookApi from '@/utils/BookApi';
 import parseError from '@/utils/parseError';
 
-function* sendCredentials(payload) {
+function* sendCredentials(payload, meta) {
+  const { resetForm, setErrors, setSubmitting } = meta;
+
   try {
     const token = yield call(BookApi.login, payload);
 
     yield put(loginSuccess());
-    yield call(BookApi.storeToken, token.access_token);
+    yield call(BookApi.storeToken, token);
+    yield call(resetForm);
     yield put(closeLoginModal());
     yield put(getAuthenticatedUserPending());
     
   } catch(error) {
-    const errorReponse = parseError(error);
+    const errors = parseError(error);
+
+    yield call(setSubmitting, false);
+
+    if (isObject(errors)) {
+      yield call(setErrors, errors);
+      yield put(loginError());
+
+      return;
+    }
     
-    yield put(loginError(errorReponse));
+    yield put(loginError({ errors }));
+
   } finally {
     if (yield cancelled()) {
-      //Action returns to original values
       yield put(loginSuccess);
     }
   }
@@ -29,9 +42,9 @@ function* sendCredentials(payload) {
 
 export default function* loginFlow() {
   while(true) {
-    const { payload } = yield take(LOGIN_USER_ASYNC.PENDING);
+    const { payload, meta } = yield take(LOGIN_USER_ASYNC.PENDING);
 
-    const task = yield fork(sendCredentials, payload);
+    const task = yield fork(sendCredentials, payload, meta);
     const action = yield take([LOGOUT, LOGIN_USER_ASYNC.ERROR]);
 
     if (isEqual(action.type, LOGOUT)) {
