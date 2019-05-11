60edf549b400ec6dd8dfa1c3fdea4e8e37655275
Wilmerq <wilmer.payall@gmail.com>
2019-05-10 23:49:18 -0400

    add refresh token async token

--- src/store/login/types.js
+++ src/store/login/types.js
@@ -1,6 +1,7 @@
 import { createAsyncTypes } from '@/store/utils';
 
 export const LOGIN_USER_ASYNC = createAsyncTypes('LOGIN_USER');
+export const REFRESH_TOKEN_ASYNC = createAsyncTypes('REFRESH_TOKEN');
 export const OPEN_LOGIN_MODAL = 'OPEN_LOGIN_MODAL';
 export const CLOSE_LOGIN_MODAL = 'CLOSE_LOGIN_MODAL';
 export const LOGOUT = 'LOGOUT';
