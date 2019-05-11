0bd0f2c9158de6c927c358a445fe1fcad3c0284e
Wilmerq <wilmer.payall@gmail.com>
2019-05-10 22:17:05 -0400

    Import refreshTokenSaga

--- src/store/login/sagas.js
+++ src/store/login/sagas.js
@@ -1,8 +1,10 @@
 import { fork } from 'redux-saga/effects';
 import loginSaga from './sagas/loginSaga';
 import logoutSaga from './sagas/logoutSaga';
+import refreshTokenSaga from './sagas/refreshTokenSaga';
 
 export default function* root() {
   yield fork(loginSaga);
   yield fork(logoutSaga);
+  yield fork(refreshTokenSaga);
 }
