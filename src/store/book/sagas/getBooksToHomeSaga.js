d2de7c2b49daca442f9b818f7e9aa3ef767d6d0b
Wilmerq <wilmer.payall@gmail.com>
2019-05-14 21:38:28 -0400

    use empty object with merge lodash function q q

--- src/store/book/sagas/getBooksToHomeSaga.js
+++ src/store/book/sagas/getBooksToHomeSaga.js
@@ -1,6 +1,5 @@
 import { take, call, put, all, fork, cancel } from 'redux-saga/effects';
 import BookApi from '@/utils/BookApi';
-import isEmpty from 'lodash/isEmpty';
 import merge from 'lodash/merge';
 
 import { 
@@ -18,15 +17,15 @@ import {
 
 function* getBooksToHome({ categoriesIds, lastSearch }) {
   try {
-    const params = { include: 'user '};
-
+    const params = { include: 'user' };
+    
     const [booksMoreSeen, booksByCategories] = yield all([
       call(BookApi.getBooksToHome, params),
-      call(BookApi.getBooksToHome, merge(params, { categoriesIds }))
+      call(BookApi.getBooksToHome, merge({}, params, { categoriesIds }))
     ]);
 
-    if (!isEmpty(lastSearch)) {
-      const booksByLastSearch = yield call(BookApi.getBooksToHome, merge(params, { searchName: lastSearch }));
+    if (!lastSearch) {
+      const booksByLastSearch = yield call(BookApi.getBooksToHome, merge({}, params, { searchName: lastSearch }));
 
       yield put(putBooksByLastSearch(booksByLastSearch));
     }
@@ -43,7 +42,7 @@ function* getBooksToHome({ categoriesIds, lastSearch }) {
 export default function* watchGetBooksHome() {
   while(true) {
     const { payload } = yield take(GET_BOOKS_TO_HOME_ASYNC.PENDING);
-    const getBooksToHomeTask =  yield fork(getBooksToHome, payload);
+    const getBooksToHomeTask = yield fork(getBooksToHome, payload);
 
     yield take(CANCEL_GET_BOOKS_TO_HOME_ASYNC);
     yield cancel(getBooksToHomeTask);
