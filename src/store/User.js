bd5981f6a53115ec100729f1bfc6158bde2f6de2
Wilmerq <wilmer.payall@gmail.com>
2019-05-21 18:23:49 -0400

    Add editUserPending action

--- src/store/User.js
+++ src/store/User.js
@@ -2,7 +2,12 @@
 * User Actions
 */
 import { createAction } from 'redux-actions';
-import { STORE_USER_ASYNC, STORE_USER_RESET } from './user/types';
+import { 
+  STORE_USER_ASYNC, 
+  EDIT_USER_ASYNC, 
+  RESET_STORE_USER, 
+  RESET_EDIT_USER 
+} from './user/types';
 
 export const storeUserPending = createAction(
   STORE_USER_ASYNC.PENDING, 
@@ -12,4 +17,14 @@ export const storeUserPending = createAction(
 
 export const storeUserError = createAction(STORE_USER_ASYNC.ERROR);
 export const storeUserSuccess = createAction(STORE_USER_ASYNC.SUCCESS);
-export const storeUserReset = createAction(STORE_USER_RESET);
+export const resetStoreUser = createAction(RESET_STORE_USER);
+
+export const editUserPending = createAction(
+  EDIT_USER_ASYNC.PENDING, 
+  ({ values }) => values,
+  ({ actions }) => actions
+);
+
+export const editUserError = createAction(EDIT_USER_ASYNC.ERROR);
+export const editUserSuccess = createAction(EDIT_USER_ASYNC.SUCCESS);
+export const resetEditUser = createAction(RESET_EDIT_USER);
