7103b32be29a7455d28a720c8d40409d7d6ed68f
Wilmerq <wilmer.payall@gmail.com>
2019-05-21 18:09:39 -0400

    Remove Router Component

--- src/app/layout/Content.js
+++ src/app/layout/Content.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import Router from './Router';
+import { Switch, Route } from 'react-router-dom';
+import Home from '@/app/views/Home';
+import Page from '@/app/views/Page/Show';
+import UserNew from '@/app/views/User/New';
+import UserEdit from '@/app/views/User/Edit';
 
-const Content = () => <section className="section">
-  <Router />
+const Content = () => <section 
+  className="section"
+>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exac path="/book/:bookId/story/:pageId?" component={Page} />
+    <Route exac path="/register" component={UserNew} />
+    <Route exac path="/profile/edit" component={UserEdit} />
+  </Switch>
 </section>;
   
 export default Content;
