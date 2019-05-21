1f861d1a49fd5d4407fcf0e13c05f5e99dad9097
Wilmerq <wilmer.payall@gmail.com>
2019-05-21 18:37:21 -0400

    Add edit user action types

--- src/store/user/types.js
+++ src/store/user/types.js
@@ -1,4 +1,6 @@
 import { createAsyncTypes } from '@/store/utils';
 
 export const STORE_USER_ASYNC = createAsyncTypes('STORE_USER');
-export const STORE_USER_RESET = 'STORE_USER_RESET';
+export const RESET_STORE_USER = 'RESET_STORE_USER';
+export const EDIT_USER_ASYNC = createAsyncTypes('EDIT_USER');
+export const RESET_EDIT_USER  = 'RESET_EDIT_USER';
