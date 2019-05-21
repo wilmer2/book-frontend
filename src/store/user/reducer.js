573a4db311854587ffcf0af3e7303002e00f17f7
Wilmerq <wilmer.payall@gmail.com>
2019-05-21 18:30:44 -0400

    Add edit user state

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -1,12 +1,20 @@
 import typeToReducer from 'type-to-reducer';
 import { fromJS } from 'immutable';
-import { STORE_USER_ASYNC, STORE_USER_RESET } from './types';
+import { STORE_USER_ASYNC, EDIT_USER_ASYNC, RESET_STORE_USER } from './types';
 import { createResolver } from '@/store/utils';
 
-const storeResolver =  createResolver('store');
+const storeResolver = createResolver('store');
+const editResolver = createResolver('edit');
 
 const initialState = fromJS({
   store: {
+    isFetching: false,
+    fetched: false,
+    fetchError: false,
+    errorMessage: '',
+  },
+
+  edit: {
     isFetching: false,
     fetched: false,
     fetchError: false,
@@ -18,7 +26,10 @@ const reducer = typeToReducer({
   [STORE_USER_ASYNC.SUCCESS]: (state, { payload }) => storeResolver.success(state, payload),
   [STORE_USER_ASYNC.PENDING]: state => storeResolver.pending(state),
   [STORE_USER_ASYNC.ERROR]: (state, { payload }) => storeResolver.error(state, payload),
-  [STORE_USER_RESET]: (state, { payload }) => storeResolver.reset(state, payload),
+  [RESET_STORE_USER]: (state, { payload }) => storeResolver.reset(state, payload),
+  [EDIT_USER_ASYNC.SUCCESS]: (state, { payload }) => editResolver.success(state, payload),
+  [EDIT_USER_ASYNC.PENDING]: state => editResolver.pending(state),
+  [EDIT_USER_ASYNC.ERROR]: (state, { payload }) => editResolver.error(state, payload),
 }, initialState);
 
 export default reducer;
