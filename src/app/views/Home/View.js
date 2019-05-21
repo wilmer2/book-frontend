e5a356630b6dfef99e56cb847dc371a6b8b390f1
Wilmerq <wilmer.payall@gmail.com>
2019-05-21 18:19:09 -0400

    Remove category state

--- src/app/views/Home/View.js
+++ src/app/views/Home/View.js
@@ -5,7 +5,6 @@ import BookList from './BookList';
 import { getBooksSelector } from '@/selectors/booksSelector';
 import { getCategoriesSelector } from '@/selectors/categoriesSelector';
 import { getAuthenticatedUserSelector }  from '@/selectors/usersSelector';
-import { getCategoriesPending } from '@/store/Category';
 
 import { 
   getBooksToHomePending, 
@@ -31,19 +30,16 @@ const getBooksIdsByLastSearch = state => state.ui.book.getIn(['homeData', 'books
 
 const mapStateToProps = (state) => {
   const homeData = state.ui.book.get('homeData');
-  const categoryData = state.ui.category;
   const categoriesIds = getCategoriesIds(state);
   const booksMoreSeen = getBooksSelector(state, getBooksIdsMoreSeen);
   const booksByCategories = getBooksSelector(state, getBooksIdsByCategories);
   const booksByLastSearch = getBooksSelector(state, getBooksIdsByLastSearch);
-  const fetchError = homeData.get('fetchError') || categoryData.get('fetchError');
-  const isFetching = homeData.get('isFetching') || categoryData.get('isFetching');
   
   return {
     fetched: homeData.get('fetched'), 
     lastSearch: homeData.get('lastSearch'),
-    isFetching,
-    fetchError,
+    isFetching: homeData.get('isFetching'),
+    fetchError: homeData.get('fetchError'),
     categoriesIds,
     booksMoreSeen,
     booksByCategories,
@@ -55,10 +51,6 @@ const mapDispatchToProps = dispatch => ({
   getBooks(params) {
     dispatch(cancelGetBooksToHomeAsync());
     dispatch(getBooksToHomePending(params));
-  },
-
-  getCategories() {
-    dispatch(getCategoriesPending());
   },
 
   openBookModal(params) {
