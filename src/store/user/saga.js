4200743ea77b4a8904b5b85991110873aace6151
Wilmerq <wilmer.payall@gmail.com>
2019-05-21 18:36:46 -0400

    Add edit user saga

--- src/store/user/saga.js
+++ src/store/user/saga.js
@@ -1,8 +1,14 @@
 import { fork } from 'redux-saga/effects';
 import BookApi from '@/utils/BookApi';
 import { storeDataSaga } from '@/store/sagaCreator';
-import { storeUserSuccess, storeUserError } from '@/store/User';
-import { STORE_USER_ASYNC } from './types';
+import { STORE_USER_ASYNC, EDIT_USER_ASYNC } from './types';
+import { 
+  storeUserSuccess, 
+  storeUserError, 
+  editUserSuccess, 
+  editUserError 
+} from '@/store/User';
+
 
 const storeUser = storeDataSaga(
   STORE_USER_ASYNC.PENDING,
@@ -11,6 +17,14 @@ const storeUser = storeDataSaga(
   BookApi.storeUser
 );
 
+const editUser = storeDataSaga(
+  EDIT_USER_ASYNC.PENDING,
+  editUserSuccess,
+  editUserError,
+  BookApi.editUser
+);
+
 export default function* rootSaga() {
   yield fork(storeUser);
+  yield fork(editUser);
 }
