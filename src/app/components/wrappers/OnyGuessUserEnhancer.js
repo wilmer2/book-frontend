69466931d509cf676200eb49e43f279f0ea6ed86
Wilmerq <wilmer.payall@gmail.com>
2019-05-21 18:07:47 -0400

    Remove async actions

--- src/app/components/wrappers/OnyGuessUserEnhancer.js
+++ src/app/components/wrappers/OnyGuessUserEnhancer.js
@@ -1,77 +1,28 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
-import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { refreshTokenPending } from '@/store/Login';
-import MediumSpinner from '@/app/components/ui/MediumSpinner';
 
-const mapStateToProps = (state) => {
-  const refreshTokenUI = state.ui.login.get('refreshToken');
+const onlyGuessUser = WrappedComponent => class extends Component {
+  componentDidMount() {
+    const token = localStorage.getItem('token');
 
-  return {
-    refreshFetched: refreshTokenUI.get('fetched'),
-    refreshIsFetching: refreshTokenUI.get('isFetching'),
-  };
-}
-
-const mapDispatchToProps = dispatch => ({
-  refreshToken(refreshTokenData) {
-    dispatch(refreshTokenPending(refreshTokenData));
-  },
-});
-
-const onlyGuessUser = (WrappedComponent) => {
-  class OnlyGuessUserContainer extends PureComponent {
-    componentDidMount() {
-      const refreshToken = localStorage.getItem('refreshToken');
-
-      if (refreshToken) 
-        this.props.refreshToken({ refreshToken });
-    }
-
-    componentDidUpdate() {
-      const { refreshFetched } = this.props;
-
-      if (refreshFetched)
-        this.props.history.push('/');
-    }
-
-    render() {
-      const { 
-        refreshIsFetching, 
-        refreshFetched,
-        ...passThroughProps
-      } = this.props;
-
-      return (
-        <Fragment>
-          {refreshIsFetching && <MediumSpinner />}
-          {!refreshIsFetching && !refreshFetched && <WrappedComponent
-            {...passThroughProps}
-          />}
-        </Fragment>
-      );
-    }
+    if (token) 
+      this.props.history.push('/');
   }
 
-  OnlyGuessUserContainer.propTypes = {
-    refreshIsFetching: PropTypes.bool.isRequired,
-    refreshFetched: PropTypes.bool.isRequired,
-    refreshToken: PropTypes.func.isRequired,
-  };
-
-  return OnlyGuessUserContainer;
+  render() {
+    return <WrappedComponent{...this.props}/>;
+  }
 }
 
 onlyGuessUser.propTypes = {
   WrappedComponent: PropTypes.element,
 };
 
-const enhanceComponent = compose(
+const enhancer = compose(
   withRouter,
-  connect(mapStateToProps, mapDispatchToProps),
   onlyGuessUser
 );
 
-export default enhanceComponent;
+export default enhancer;
