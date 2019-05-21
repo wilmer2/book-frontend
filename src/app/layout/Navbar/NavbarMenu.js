05fc9d8c2ed85e0e514716997debaad0449d2ed0
Wilmerq <wilmer.payall@gmail.com>
2019-05-21 18:15:58 -0400

    Remove showRenderAvatar method

--- src/app/layout/Navbar/NavbarMenu.js
+++ src/app/layout/Navbar/NavbarMenu.js
@@ -1,83 +1,56 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import isEmpty from 'lodash/isEmpty';
 import SearchField from './SearchField';
 import Avatar from './Avatar';
 import CategoryDropDown from './CategoryDropDown';
 import NavbarButtons from './NavbarButtons';
-import Modal from '@/app/components/wrappers/Modal';
-import Login from '@/app/components/forms/Login';
 
 const AvatarMenu = (props) => {
-  if (props.shouldRenderAvatar()) {
+  const { authenticatedUser } = props;
+
+  if (!isEmpty(authenticatedUser)) {
     return (
       <Avatar 
-        authenticatedUser={props.authenticatedUser}
-        fetched={props.fetched}
-        fetchError={props.fetchError}
-        isFetching={props.isFetching}
+        authenticatedUser={authenticatedUser}
         onClickLogout={props.onClickLogout}
-        onClickGetAuthenticatedUser={props.onClickGetAuthenticatedUser}
       />
     );
   }
 
-  return <NavbarButtons onClickOpenLoginModal={props.onClickOpenLoginModal} />;
+  return <NavbarButtons />;
 } 
 
 class NavbarMenu extends PureComponent {
-  shouldRenderAvatar = () =>  {
-    const { fetched, isFetching, fetchError } = this.props;
-
-    return fetched || isFetching || fetchError;
-  }
-
   render() {
+    const { authenticatedUser, categories } = this.props;
+
     return (
-      <Fragment>
-        <div className="navbar-menu is-active">
-          <div className="navbar-start">
-            <CategoryDropDown />
-            <Link to="/about" className="navbar-item">About</Link>
-            <Link to="/about" className="navbar-item">About</Link>
-          </div>
-          <div className="navbar-start">
-            <SearchField />        
-          </div>
-          <div className="navbar-start">
-            <AvatarMenu 
-              authenticatedUser={this.props.authenticatedUser}
-              fetched={this.props.fetched}
-              fetchError={this.props.fetchError}
-              isFetching={this.props.isFetching}
-              onClickLogout={this.props.onClickLogout}
-              onClickGetAuthenticatedUser={this.props.onClickGetAuthenticatedUser}
-              onClickOpenLoginModal={this.props.onClickOpenLoginModal} 
-              shouldRenderAvatar={this.shouldRenderAvatar}
-            />
-          </div>
+      <div className="navbar-menu is-active">
+        <div className="navbar-start">
+          <CategoryDropDown categories={categories} />
         </div>
-        <Modal 
-          onCloseModal={this.props.onCloseLoginModal}
-          open={this.props.openLoginModal}
-        >
-          <Login />
-        </Modal>
-      </Fragment>
+        <div className="navbar-start">
+          <SearchField />        
+        </div>
+        <div className="navbar-start">
+          <AvatarMenu 
+            authenticatedUser={authenticatedUser}
+            onClickLogout={this.props.onClickLogout}
+          />
+        </div>
+      </div>
     );
   }
 }
 
 NavbarMenu.propTypes = {
   authenticatedUser: PropTypes.object.isRequired,
-  isFetching: PropTypes.bool.isRequired,
-  fetched: PropTypes.bool.isRequired,
-  fetchError: PropTypes.bool.isRequired,
-  onClickOpenLoginModal: PropTypes.func.isRequired,
-  onCloseLoginModal: PropTypes.func.isRequired,
-  openLoginModal: PropTypes.bool.isRequired,
+  categories: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array
+  ]).isRequired,
   onClickLogout: PropTypes.func.isRequired,
-  onClickGetAuthenticatedUser: PropTypes.func.isRequired,
 };
 
 export default NavbarMenu;
