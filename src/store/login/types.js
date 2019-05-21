54b6df453a76ee8d21e9b0e1616beb4c728e10d9
Wilmerq <wilmer.payall@gmail.com>
2019-05-21 18:29:22 -0400

    Add refresh action types

--- src/store/login/types.js
+++ src/store/login/types.js
@@ -1,7 +1,8 @@
 import { createAsyncTypes } from '@/store/utils';
 
 export const LOGIN_USER_ASYNC = createAsyncTypes('LOGIN_USER');
-export const REFRESH_TOKEN_ASYNC = createAsyncTypes('REFRESH_TOKEN');
+export const REFRESH_TOKEN_START = 'REFRESH_TOKEN_START';
+export const REFRESH_TOKEN_END = 'REFRESH_TOKEN_END';
 export const OPEN_LOGIN_MODAL = 'OPEN_LOGIN_MODAL';
 export const CLOSE_LOGIN_MODAL = 'CLOSE_LOGIN_MODAL';
 export const LOGOUT = 'LOGOUT';
