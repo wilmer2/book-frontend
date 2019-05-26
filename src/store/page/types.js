dcf4d11f3f2738da152c173c5003c2a1da6f2f10
Wilmerq <wilmer.payall@gmail.com>
2019-05-26 12:17:41 -0400

    Add RESET_PAGES_PAGINATION action type

--- src/store/page/types.js
+++ src/store/page/types.js
@@ -2,4 +2,4 @@ import { createAsyncTypes } from '@/store/utils';
 
 export const GET_PAGES_ASYNC = createAsyncTypes('GET_PAGES');
 export const GET_PAGE_BY_ID_ASYNC = createAsyncTypes('GET_PAGE_BY_ID');
-export const RESET_PAGES_PAGINATION = 'RESET_PAGES_PAGINATION';
+export const RESET_PAGES_PAGINATON = 'RESET_PAGES_PAGINATON';
