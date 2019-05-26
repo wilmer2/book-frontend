f4e001b057e67f1c8e757c33ebcd1434b059318f
Wilmerq <wilmer.payall@gmail.com>
2019-05-26 12:18:57 -0400

    reducer for RESET_PAGE_PAGINATION action type

--- src/store/page/reducer.js
+++ src/store/page/reducer.js
@@ -1,16 +1,11 @@
 import typeToReducer from 'type-to-reducer';
 import { fromJS } from 'immutable';
-import { 
-  GET_PAGE_BY_ID_ASYNC, 
-  GET_PAGES_ASYNC, 
-  RESET_PAGES_PAGINATION
-} from './types';
+import { GET_PAGE_BY_ID_ASYNC, GET_PAGES_ASYNC, RESET_PAGES_PAGINATON } from './types';
 
-import { createResolver, resetPagination } from '@/store/utils';
+import { createResolver } from '@/store/utils';
 
 const resolverById = createResolver('byId');
 const pageResolver = createResolver();
-const infinityPagination = true;
 
 const initialState = fromJS({
   isFetching: true,
@@ -19,6 +14,7 @@ const initialState = fromJS({
   pagination: {
     totalPages: 1,
     currentPage: 1,
+    total: 0,
     ids: [],
   },
   byId: {
@@ -30,15 +26,17 @@ const initialState = fromJS({
 });
 
 const reducer = typeToReducer({
-  [GET_PAGE_BY_ID_ASYNC.SUCCESS]: (state, { payload }) => 
-    resolverById.successById(state, payload),
+  [GET_PAGE_BY_ID_ASYNC.SUCCESS]: (state, { payload }) => {
+    return resolverById.successById(state, payload);
+  },
   [GET_PAGE_BY_ID_ASYNC.PENDING]: state => resolverById.pending(state),
   [GET_PAGE_BY_ID_ASYNC.ERROR]: (state, { payload }) => resolverById.error(state, payload),
-  [GET_PAGES_ASYNC.SUCCESS]: (state, { payload }) => 
-    pageResolver.success(state, payload, infinityPagination),
+  [GET_PAGES_ASYNC.SUCCESS]: (state, { payload }) => {
+    return pageResolver.successInfinity(state, payload);
+  },
   [GET_PAGES_ASYNC.PENDING]: state => pageResolver.pending(state),
   [GET_PAGES_ASYNC.ERROR]: (state, { payload }) => pageResolver.error(state, payload),
-  [RESET_PAGES_PAGINATION]: state => resetPagination(state),
+  [RESET_PAGES_PAGINATON]: state => pageResolver.resetPagination(state),
 }, initialState);
 
 export default reducer;
