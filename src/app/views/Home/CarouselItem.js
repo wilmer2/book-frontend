7abfb5700efd66e97e3c710080b3033ad6f6d4df
Wilmerq <wilmer.payall@gmail.com>
2019-05-29 18:51:45 -0400

    Add breakPoints state

--- src/app/views/Home/CarouselItem.js
+++ src/app/views/Home/CarouselItem.js
@@ -1,23 +1,35 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Carousel from 'react-elastic-carousel';
 import BookItem from '@/app/components/ui/BookItem';
+import styled from 'styled-components';
+
+const CarouselWrapped = styled.div`
+  margin-bottom: 30px;
+`;
 
 class CarouselItem extends PureComponent {
+  state = {
+    breakPoints: [
+      { width: 1, itemsToShow: 1 },
+      { width: 850, itemsToShow: 3}
+    ],
+  }
+
   render() {
     const { title, books } = this.props;
 
     return (
-      <Fragment>
-        <h1 className="title">{title}</h1>
-        <Carousel>
+      <CarouselWrapped>
+        <h2 className="title">{title}</h2>
+        <Carousel breakPoints={this.state.breakPoints}>
           {books.map(book => <BookItem 
             key={`${book.id}-${title}`} 
             book={book}
             onClickBook={this.props.onClickOpenBookModal} 
           />)}
         </Carousel>    
-      </Fragment>
+      </CarouselWrapped>
     );
   }
 }
