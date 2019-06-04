72294f98a89ce21beeebc9f71f383ec93c3d77ad
Wilmerq <wilmer.payall@gmail.com>
2019-06-03 22:45:15 -0400

    Add close button

--- src/app/components/ui/ErrorGeneralMessage.js
+++ src/app/components/ui/ErrorGeneralMessage.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const ErrorGeneralHeader = (props) => {
+  if (!props.withOnCloseButton) return null;
+
+  return (
+     <div className="message-header">
+      <p>Error</p>
+      <button 
+        className="delete is-small" 
+        aria-label="delete" 
+        onClick={props.onClickClose}>
+      </button>
+    </div>
+  )
+}
 const ErrorGeneralMessage = props => <article 
   className="message is-danger"
->    
+> 
+  <ErrorGeneralHeader
+    withOnCloseButton={props.withOnCloseButton}
+    onClickClose={props.onClickClose}
+  />
   <div className="message-body">
-      <span className="is-danger">{props.errorMessage}</span>
+    <span className="is-danger">{props.errorMessage}</span>
   </div>
 </article>;
 
 ErrorGeneralMessage.propTypes = {
   errorMessage: PropTypes.string.isRequired,
+  onClickClose: PropTypes.func,
+  withOnCloseButton: PropTypes.bool,
+};
+
+ErrorGeneralMessage.defaultProps = {
+  withOnCloseButton: false,
 };
 
 export default ErrorGeneralMessage;
